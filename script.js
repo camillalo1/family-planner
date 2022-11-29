beef5a96d2acbf1bdb9a52f0f32465d0700ce3d0
camillalo1 <camillalo@getacademy.no>
2022-11-29 15:02:17 +0100

    Learning tables

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //Model 
 
+let tasks = [
+    {description: 'Handle til middag', isDone: false},
+    {description: 'Lage middag', isDone: false},
+    {description: 'Spise middag', isDone: false},
+];
+
+let taskDescriptionInput = document.getElementById('taskDescription');
 
 //View
 
@@ -12,6 +19,7 @@ function updateView(){
         <h1> Lopes-Garberg </h1>
 
         <tr>
+            <th style = "visbility: collapse"></th>
             <th> Mandag </th>
             <th> Tirsdag </th>
             <th> Onsdag </th>
@@ -19,12 +27,63 @@ function updateView(){
             <th> Fredag </th>
             <th> Lørdag </th>
             <th> Søndag </th>
-        </tr>;
+        </tr>
+        <tr>
+            <th> Arnstein </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th> Camilla </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th> Olivia </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th> Marve </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <br/>
+            
+            
+            <div>
+                <p> Ting som må gjøres i løpet av uka: </p>
+                <input id = "taskDescription" type = "text" />
+                <button onclick = "addTask()"> Legg til oppgave </button>
+            </div>
 
-        for (let i = 0; i < tasks.length; i++) {
-            html += createHtmlRow (i);
-        }
-    `
+            <div>
+                <p> Viktige datoer: </p>
+                <input id = "importantDate" type = "text" />
+                <button onclick = "addDate()"> Legg til dato </button>
+            </div>
+            `;
+
     planTable.innerHTML = html;
 }
 
